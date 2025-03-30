933dd88540d06795d8976dd04aa623365f679036
santiesleo <83185169+santiesleo@users.noreply.github.com>
2025-03-30 15:35:05 -0500

    fix: role guard

--- app/access-denied/page.tsx
+++ app/access-denied/page.tsx
@@ -8,7 +8,7 @@ export default function AccessDeniedPage() {
     <>
       <Navbar />
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white p-4">
-        <h1 className="text-4xl font-bold mb-4 text-red-500">Acceso Denegado</h1>
+        <h1 className="text-4xl font-bold mb-4 text-red-500">Acceso denegado</h1>
         <p className="text-xl mb-8">
           No tienes permiso para acceder a esta página.
         </p>
