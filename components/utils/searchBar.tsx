1225b6f464c92be5f503ff2561d5b73b7815cd3d
Dantesiio <DavidDo29@hotmail.com>
2025-04-05 20:01:29 -0500

    deletenavbar

--- components/utils/searchBar.tsx
+++ components/utils/searchBar.tsx
@@ -13,14 +13,7 @@ const SearchBar = () => {
 
   return (
     <div className="flex items-center space-x-2">
-      <Input 
-        type="text" 
-        placeholder="Buscar..." 
-        value={query} 
-        onChange={(e) => setQuery(e.target.value)}
-        className="w-64 bg-secondary"
-      />
-      <Button onClick={handleSearch} className='bg-primary-darker'>Buscar</Button>
+      
     </div>
   );
 };
