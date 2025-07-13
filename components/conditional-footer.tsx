4eeafc83c9d79c5239a589f504f4882055292aea
DiegoZM1006 <1109184718@u.icesi.edu.co>
2025-07-13 11:24:11 -0500

    fix: add footer to all pages

---
+++ components/conditional-footer.tsx
@@ -0,0 +1,14 @@
+"use client"
+import { usePathname } from "next/navigation"
+import Footer from "./footer"
+
+export default function ConditionalFooter() {
+  const pathname = usePathname()
+  const isAuthPage = pathname?.startsWith('/auth')
+  
+  if (isAuthPage) {
+    return null
+  }
+  
+  return <Footer />
+}
